ac4e5da05a4922bb6faf4c4e6f69649ce331878f
KouhouMed <mohamedkouhou@gmail.com>
2024-08-31 10:47:52 +0200

    fixed flake8 linting errors

--- src/web/routes.py
+++ src/web/routes.py
@@ -11,6 +11,7 @@ def rate_limit(limit_per_minute):
     def decorator(f):
         last_request_time = {}
         @wraps(f)
+
         def wrapped(*args, **kwargs):
             now = time.time()
             if request.remote_addr in last_request_time:
