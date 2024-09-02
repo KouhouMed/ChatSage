228f5abcc318f4639a111c2b351d0e69be9bed1c
KouhouMed <mohamedkouhou@gmail.com>
2024-09-02 21:48:34 +0200

    flake8 and black routine

--- src/models.py
+++ src/models.py
@@ -4,12 +4,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-    chats = db.relationship('Chat', backref='user', lazy='dynamic')
+    chats = db.relationship("Chat", backref="user", lazy="dynamic")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -17,15 +18,17 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    messages = db.relationship('Message', backref='chat', lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    messages = db.relationship("Message", backref="chat", lazy="dynamic")
+
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=db.func.now())
-    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'))
+    chat_id = db.Column(db.Integer, db.ForeignKey("chat.id"))
     is_user = db.Column(db.Boolean, default=True)
