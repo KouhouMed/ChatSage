8f77376eb902b5e61f8b2c689a2fa5321261ccb3
KouhouMed <mohamedkouhou@gmail.com>
2024-09-04 16:12:18 +0200

    registration forms

--- src/forms.py
+++ src/forms.py
@@ -7,3 +7,11 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
